3f4b708c0fb31db7c7b246a43db58f120a2bd700
Yazan Khayata <yazankhayata@Yazans-MacBook-Air.local>
2020-12-06 21:38:56 -0800

    Adding Python files.

---
+++ Python_practice.py
@@ -0,0 +1,14 @@
+counties = ["Arapahoe", "Denver", "Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties")
+else: 
+    print("El Paso is not in the list of counties.")
+
+for county in counties:
+    print(county)
+
+
+
+
+
+
